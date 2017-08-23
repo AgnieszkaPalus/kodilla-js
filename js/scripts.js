4495a07cca769eb346b3254c6f4db668a8c44b2d
Agnieszka Palus <a.palus@gmail.com>
2017-08-23 22:22:40 +0200

    comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 // scripts.js
 
 var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+// adding comment
+
+/* 
+var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+*/
