aa9ef35abb2fb3a2f996fca9c94c6b30314b5c7a
Agnieszka Palus <a.palus@gmail.com>
2017-08-29 15:31:55 +0200

    variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
 // scripts.js
+// zadanie 8.6
+var a = 3;
+	b = 4;
+	value = (a * a) + (2 * a * b) - (b * b);
+	
+
+if (value > 0) {
+	console.log('Value is ' + value + ", and it's a positive value")
+} 
+else if (value < 0) {
+	console.log('Value is ' + value + ", and it's a negative value")
+}
+
+
+
+if (value == 0) {
+	alert('Value is a zero!')
+}
+
+
+
+
+
+
+
+
+/*
+Zadanie 8.5
 
 var a = 5;
 	h = 7;
 	triangleArea = a*h/2;
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+*/
