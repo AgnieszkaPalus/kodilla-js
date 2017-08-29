ef031df44f0ddd082dbb2360efb23a2a75f18649
Agnieszka Palus <a.palus@gmail.com>
2017-08-29 14:14:16 +0200

    variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,7 @@
 // scripts.js
 
-var name = prompt('Enter your name');
-//alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var a = 5;
+	h = 7;
+	triangleArea = a*h/2;
 
-// adding comment
-
-/* 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
-*/
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
